c922f6a3ee4bb2e953670ca977eb34e394fc11d7
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-01-01 13:18:45 +0800

    new package: python3.10-scipy-1.9.3

---
+++ tur-pypi/python3.10-scipy-1.9.3/build.sh
@@ -0,0 +1,132 @@
+TERMUX_PKG_HOMEPAGE=https://scipy.org/
+TERMUX_PKG_DESCRIPTION="Fundamental algorithms for scientific computing in Python"
+TERMUX_PKG_LICENSE="BSD 3-Clause"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION=1.9.3
+TERMUX_PKG_SRCURL=https://github.com/scipy/scipy.git
+TERMUX_PKG_DEPENDS="libc++, libopenblas, python3.10, python3.10-numpy-1.24.1"
+TERMUX_PKG_BUILD_DEPENDS="python3.10-numpy-1.24.1-static"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
+
+# Tests will hang on arm and will failed with `Segmentation fault` on i686.
+# See https://github.com/termux-user-repository/tur/pull/21#issue-1295483266.
+# 
+# The logs of this crash on i686 are as following. 
+# linalg/tests/test_basic.py: Fatal Python error: Segmentation fault
+# 
+# Current thread 0xf7f4b580 (most recent call first):
+#   File "/data/data/com.termux/files/usr/lib/python3.10/site-packages/scipy-1.8.0-py3.10-linux-i686.egg/scipy/linalg/_basic.py", line 1227 in lstsq
+#   File "/data/data/com.termux/files/usr/lib/python3.10/site-packages/scipy-1.8.0-py3.10-linux-i686.egg/scipy/linalg/tests/test_basic.py", line 1047 in test_simple_overdet_complex
+# XXX: Although it doesn't seem to work fine, I'd like to enable this package as it happens only on some functions.
+# TERMUX_PKG_BLACKLISTED_ARCHES="arm, i686"
+
+TERMUX_PKG_RM_AFTER_INSTALL="
+bin/
+"
+
+source $TERMUX_SCRIPTDIR/common-files/setup_toolchain_gcc.sh
+source $TERMUX_SCRIPTDIR/common-files/tur_elf_cleaner_for_wheel.sh
+
+termux_step_configure() {
+	if $TERMUX_ON_DEVICE_BUILD; then
+		termux_error_exit "Package '$TERMUX_PKG_NAME' is not available for on-device builds."
+	fi
+
+	_PYTHON_VERSION=3.10
+	_NUMPY_VERSION=1.24.1
+
+	_setup_toolchain_ndk_gcc_11
+
+	# XXX: `python` from main repo is built by TERMUX_STANDALONE_TOOLCHAIN and its _sysconfigdata.py
+	# XXX: contains some FLAGS which is not supported by GNU Compiler Collections, such as '-Oz',  
+	# XXX: `-static-openmp`. So we need to modify the _sysconfigdata.py.
+	SYS_CONFIG_DATA_FILE="$(find $TERMUX_PREFIX/lib/python${_PYTHON_VERSION} -name "_sysconfigdata*.py")"
+	rm -rf  $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/__pycache__
+	cp $SYS_CONFIG_DATA_FILE $TERMUX_PKG_TMPDIR/$(basename $SYS_CONFIG_DATA_FILE)
+	sed -E 's|-O[123sz]|-Os|g;s|-static-openmp||g' $TERMUX_PKG_TMPDIR/$(basename $SYS_CONFIG_DATA_FILE) |
+		sed "s|$TERMUX_HOST_PLATFORM-clang++|$TERMUX_HOST_PLATFORM-g++|g" |
+		sed "s|$TERMUX_HOST_PLATFORM-clang|$TERMUX_HOST_PLATFORM-gcc|g" > $SYS_CONFIG_DATA_FILE
+	rm $TERMUX_PKG_TMPDIR/$(basename $SYS_CONFIG_DATA_FILE)
+
+	termux_setup_python_crossenv
+	pushd $TERMUX_PYTHON_CROSSENV_SRCDIR
+	_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python-crossenv-prefix
+	python${_PYTHON_VERSION} -m crossenv \
+		$TERMUX_PREFIX/bin/python${_PYTHON_VERSION} \
+		${_CROSSENV_PREFIX}
+	popd
+	. ${_CROSSENV_PREFIX}/bin/activate
+
+	LDFLAGS+=" -Wl,--no-as-needed,-lpython${_PYTHON_VERSION}"
+}
+
+termux_step_make() {
+	pip --no-cache-dir install wheel
+	build-pip install numpy==$_NUMPY_VERSION pybind11 Cython pythran wheel
+
+	DEVICE_SITE=$TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/site-packages
+
+	# From https://gist.github.com/benfogle/85e9d35e507a8b2d8d9dc2175a703c22
+	BUILD_SITE=${_CROSSENV_PREFIX}/build/lib/python${_PYTHON_VERSION}/site-packages
+	INI=$(find $BUILD_SITE -name 'npymath.ini')
+	LIBDIR=$(find $DEVICE_SITE -path '*/numpy/core/lib')
+	INCDIR=$(find $DEVICE_SITE -path '*/numpy/core/include')
+	cat <<-EOF > $INI 
+	[meta]
+	Name=npymath
+	Description=Portable, core math library implementing C99 standard
+	Version=0.1
+	[variables]
+	# Force it to find cross-build libs when we build scipy
+	libdir=$LIBDIR
+	includedir=$INCDIR
+	[default]
+	Libs=-L\${libdir} -lnpymath
+	Cflags=-I\${includedir}
+	Requires=mlib
+	EOF
+
+	cp $DEVICE_SITE/numpy/core/lib/libnpymath.a $TERMUX_PREFIX/lib
+	cp $DEVICE_SITE/numpy/random/lib/libnpyrandom.a $TERMUX_PREFIX/lib
+
+	cat <<- EOF > site.cfg
+	[openblas]
+	libraries = openblas
+	library_dirs = $TERMUX_PREFIX/lib
+	include_dirs = $TERMUX_PREFIX/include
+	EOF
+
+	# XXX: It's hard for pip to work, as scipy uses meson-python as backend.
+	# XXX: More investigations are needed.
+	F90=$FC F77=$FC python setup.py bdist_wheel -v
+}
+
+termux_step_make_install() {
+	export PYTHONPATH="$DEVICE_SITE"
+	F90=$FC F77=$FC pip install ./dist/*.whl --no-deps --prefix=$TERMUX_PREFIX
+}
+
+termux_step_post_make_install() {
+	# Remove these dummy files.
+	rm $TERMUX_PREFIX/lib/libnpymath.a
+	rm $TERMUX_PREFIX/lib/libnpyrandom.a
+	# Remove __pycache__ and _sysconfigdata.py
+	rm $SYS_CONFIG_DATA_FILE
+	rm -rf $TERMUX_PREFIX/lib/python${_PYTHON_VERSION}/__pycache__
+}
+
+termux_step_post_massage() {
+	pushd $TERMUX_PKG_BUILDDIR
+
+	# Run elf-cleaner for wheels
+	shopt -s nullglob
+	local _whl
+	for _whl in ./dist/*.whl; do
+		tur_elf_cleaner_for_wheel $_whl
+	done
+	shopt -u nullglob
+
+	cp ./dist/*.whl $TERMUX_SCRIPTDIR/output/
+	popd
+}

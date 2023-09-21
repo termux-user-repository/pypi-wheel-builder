d4256ad9491eadadff5164355f9b47d98c021bcd
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-09-22 02:51:53 +0800

    new package: python3.7-mitmproxy-wireguard

---
+++ tur-pypi/python3.7-mitmproxy-wireguard/build.sh
@@ -0,0 +1,59 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/decathorpe/mitmproxy_wireguard
+TERMUX_PKG_DESCRIPTION="WireGuard frontend for mitmproxy"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="0.1.19"
+TERMUX_PKG_SRCURL=https://github.com/decathorpe/mitmproxy_wireguard/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=749b5b45222b629f4cced154cc4bf70ba7ae3061db02e2ea0ae45a4ae6246463
+TERMUX_PKG_DEPENDS="libc++, openssl, python3.7"
+TERMUX_PKG_BUILD_DEPENDS="python3.7-cross"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="newest-tag"
+
+TERMUX_PYTHON_VERSION=3.7
+TERMUX_PYTHON_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python37-crossenv-prefix-$TERMUX_ARCH
+
+TUR_AUTO_AUDIT_WHEEL=true
+TUR_AUTO_BUILD_WHEEL=false
+TUR_WHEEL_DIR="target/wheels"
+
+source $TERMUX_SCRIPTDIR/common-files/tur_elf_cleaner_for_wheel.sh
+
+termux_step_post_get_source() {
+	export PATH="$TERMUX_PREFIX/opt/python$TERMUX_PYTHON_VERSION/cross/bin:$PATH"
+}
+
+termux_step_pre_configure() {
+	termux_setup_rust
+
+	termux_setup_python_pip
+
+	build-pip install maturin
+}
+
+termux_step_make_install() {
+	export CARGO_BUILD_TARGET=${CARGO_TARGET_NAME}
+	export PYO3_CROSS_LIB_DIR=$TERMUX_PREFIX/lib
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
+
+	build-python -m maturin build --release --skip-auditwheel --target $CARGO_BUILD_TARGET
+
+	# Fix wheel name for arm
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		mv ./target/wheels/mitmproxy_wireguard-$TERMUX_PKG_VERSION-cp37-abi3-linux_armv7l.whl \
+			./target/wheels/mitmproxy_wireguard-$TERMUX_PKG_VERSION-py37-none-any.whl
+	fi
+
+	pip install --no-deps ./target/wheels/*.whl --prefix $TERMUX_PREFIX
+
+	# Fix wheel name, although it it built with tag `cp37-abi3`, but it is linked against `python3.7m.so`
+	# so it will not work on other pythons.
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		mv ./target/wheels/mitmproxy_wireguard-$TERMUX_PKG_VERSION-py37-none-any.whl \
+			./target/wheels/mitmproxy_wireguard-$TERMUX_PKG_VERSION-cp37-cp37-linux_armv7l.whl
+	else
+		mv ./target/wheels/mitmproxy_wireguard-$TERMUX_PKG_VERSION-cp37-abi3-linux_$TERMUX_ARCH.whl \
+			./target/wheels/mitmproxy_wireguard-$TERMUX_PKG_VERSION-cp37-cp37-linux_$TERMUX_ARCH.whl
+	fi
+}

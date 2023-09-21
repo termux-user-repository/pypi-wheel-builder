39e8df9f77f18fffa51d22d6ef53ca6fce8f0a75
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-09-22 05:03:53 +0800

    python3.9-numpy: bump to 1.26.0

--- tur-pypi/python3.9-numpy/build.sh
+++ tur-pypi/python3.9-numpy/build.sh
@@ -2,12 +2,13 @@ TERMUX_PKG_HOMEPAGE=https://numpy.org/
 TERMUX_PKG_DESCRIPTION="The fundamental package for scientific computing with Python"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="1.24.2"
+TERMUX_PKG_VERSION="1.26.0"
 TERMUX_PKG_SRCURL=git+https://github.com/numpy/numpy
 TERMUX_PKG_DEPENDS="libc++, libopenblas, python3.9"
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, pybind11, 'Cython<3', pythran"
+TERMUX_PKG_PYTHON_COMMON_DEPS="'setuptools==59.2.0', 'wheel==0.38.1', 'Cython>=0.29.34,<3.1'"
 TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PKG_RM_AFTER_INSTALL="
 bin/
 "
@@ -20,6 +21,12 @@ source $TERMUX_SCRIPTDIR/common-files/tur_elf_cleaner_for_wheel.sh
 
 termux_step_post_get_source() {
 	export PATH="$TERMUX_PREFIX/opt/python$TERMUX_PYTHON_VERSION/cross/bin:$PATH"
+
+	# numpy has switched to use meson-python by default, 
+	# but meson-python hasn't supported cross compiling.
+	# See mesonbuild/meson-python#321
+	rm -f meson.build pyproject.toml
+	mv pyproject.toml.setuppy pyproject.toml
 }
 
 termux_step_pre_configure() {

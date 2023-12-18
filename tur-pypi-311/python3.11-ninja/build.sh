9ac0d918dae72264b22c7198afb81774e851af92
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-12-19 02:24:22 +0800

    python3.11-ninja: fix auto update
    [skip ci]

--- tur-pypi-311/python3.11-ninja/build.sh
+++ tur-pypi-311/python3.11-ninja/build.sh
@@ -39,6 +39,7 @@ termux_pkg_auto_update() {
 	local new_ninja_version="$(echo $latest_tag | cut -d'.' -f1-3)"
 
 	if [[ "$new_ninja_version" != "$_NINJA_VERSION" ]]; then
+ 		_NINJA_VERSION="$new_ninja_version"
 		local tmpdir=$(mktemp -d)
 		local ninja_source_sha256
 		curl -LC - "$_NINJA_SRCURL" -o "${tmpdir}/${new_ninja_version}.tar.gz"

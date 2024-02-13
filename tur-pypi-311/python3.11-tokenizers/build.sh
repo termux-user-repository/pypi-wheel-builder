c2247ce94406b5523bbc00a377993c918824e701
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-02-14 00:38:16 +0800

    new package: python3.11-tokenizers

---
+++ tur-pypi-311/python3.11-tokenizers/build.sh
@@ -0,0 +1,67 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/huggingface/tokenizers
+TERMUX_PKG_DESCRIPTION="Fast State-of-the-Art Tokenizers optimized for Research and Production"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="0.15.2"
+TERMUX_PKG_SRCURL=https://github.com/huggingface/tokenizers/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=59b3fd70df18cf02349cdddbddc669409e4dae0d3de30fb449d1a83ba02007fd
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, python, python-pip"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
+TERMUX_PKG_PYTHON_BUILD_DEPS="maturin"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PYTHON_VERSION=3.11
+TERMUX_PYTHON_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python${TERMUX_PYTHON_VERSION/./}-crossenv-prefix-$TERMUX_ARCH
+TUR_AUTO_AUDIT_WHEEL=true
+TUR_AUDIT_WHEEL_NO_LIBS=true
+TUR_AUTO_BUILD_WHEEL=false
+TUR_WHEEL_DIR="target/wheels"
+
+termux_step_post_get_source() {
+	rm -f $TERMUX_PKG_SRCDIR/Makefile
+}
+
+termux_step_pre_configure() {
+	TERMUX_PKG_SRCDIR+="/bindings/python"
+	TERMUX_PKG_BUILDDIR+="/bindings/python"
+
+	termux_setup_rust
+
+	# Tokenizers uses some extra libs that requires `core` crates, but
+	# the toolchain provided by rustup doesn't have them (Android is at 
+	# tier 2). Use nightly toolchain and enable `build-std` feature to
+	# build these crates.
+	rustup toolchain install nightly
+	rustup component add rust-src --toolchain nightly
+	echo "nightly" > $TERMUX_PKG_SRCDIR/rust-toolchain
+}
+
+termux_step_make() {
+	:
+}
+
+termux_step_make_install() {
+	export CARGO_BUILD_TARGET=${CARGO_TARGET_NAME}
+	export PYO3_CROSS_PYTHON_VERSION=$TERMUX_PYTHON_VERSION
+	export PYO3_CROSS_LIB_DIR=$TERMUX_PREFIX/lib
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
+
+	export RUSTFLAGS="-C link-args=-L$TERMUX_PREFIX/lib $RUSTFLAGS"
+
+	build-python -m maturin build --release --skip-auditwheel --target $CARGO_BUILD_TARGET -Z build-std
+
+	# Fix wheel name for arm
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		mv ./target/wheels/tokenizers-$TERMUX_PKG_VERSION-cp311-cp311-linux_armv7l.whl \
+			./target/wheels/tokenizers-$TERMUX_PKG_VERSION-py311-none-any.whl
+	fi
+
+	pip install --no-deps ./target/wheels/*.whl --prefix $TERMUX_PREFIX 
+
+	# Fix wheel name for arm
+	if [ "$TERMUX_ARCH" = "arm" ]; then
+		mv ./target/wheels/tokenizers-$TERMUX_PKG_VERSION-py311-none-any.whl \
+			./target/wheels/tokenizers-$TERMUX_PKG_VERSION-cp311-cp311-linux_armv7l.whl
+	fi
+}

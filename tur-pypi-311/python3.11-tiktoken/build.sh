625d5c6891a9bd885dc9da1a38a48724c5100d48
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-02-14 01:02:24 +0800

    new package: python3.11-tiktoken

---
+++ tur-pypi-311/python3.11-tiktoken/build.sh
@@ -0,0 +1,28 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/openai/tiktoken
+TERMUX_PKG_DESCRIPTION="A fast BPE tokeniser for use with OpenAI's models"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="0.6.0"
+TERMUX_PKG_SRCURL=https://github.com/openai/tiktoken/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=d40bf4bf4a32bda814a16b2c5651663960da40ac5a8c02a65b7a9aa97bda2046
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, python"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, setuptools-rust"
+TERMUX_PKG_PYTHON_TARGET_DEPS="'regex>=2022.1.18', 'requests>=2.26.0'"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PYTHON_VERSION=3.11
+TERMUX_PYTHON_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python${TERMUX_PYTHON_VERSION/./}-crossenv-prefix-$TERMUX_ARCH
+TUR_AUTO_AUDIT_WHEEL=true
+TUR_AUDIT_WHEEL_NO_LIBS=true
+
+source $TERMUX_SCRIPTDIR/common-files/tur_build_wheel.sh
+
+termux_step_pre_configure() {
+	termux_setup_rust
+	export CARGO_BUILD_TARGET=${CARGO_TARGET_NAME}
+	export PYO3_CROSS_LIB_DIR=$TERMUX_PREFIX/lib
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
+
+	LDFLAGS+=" -Wl,--no-as-needed -lpython${TERMUX_PYTHON_VERSION}"
+}

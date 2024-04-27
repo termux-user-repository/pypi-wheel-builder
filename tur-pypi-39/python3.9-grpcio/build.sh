9f80df32fffc7b0f7e88919d58ff2f55ca2b65d4
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-04-27 17:06:12 +0800

    python3.9-grpcio: bump to 1.63.0

--- tur-pypi-39/python3.9-grpcio/build.sh
+++ tur-pypi-39/python3.9-grpcio/build.sh
@@ -3,9 +3,9 @@ TERMUX_PKG_DESCRIPTION="High performance, open source, general RPC framework tha
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
 TERMUX_PKG_SRCURL=git+https://github.com/grpc/grpc
-TERMUX_PKG_VERSION="1.62.1"
-TERMUX_PKG_DEPENDS="abseil-cpp, c-ares, ca-certificates, libc++, libre2, openssl, python3.9, zlib"
-TERMUX_PKG_BUILD_DEPENDS="gflags, gflags-static"
+TERMUX_PKG_VERSION="1.63.0"
+TERMUX_PKG_DEPENDS="ca-certificates, libc++, openssl, python3.9, zlib"
+TERMUX_PKG_BUILD_DEPENDS="gflags, gflags-static, python3.9-cross"
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, 'setuptools==65.4.1', 'Cython<3'"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_AUTO_UPDATE=true
@@ -14,6 +14,7 @@ TERMUX_PKG_UPDATE_TAG_TYPE="latest-release-tag"
 TERMUX_PYTHON_VERSION=3.9
 TERMUX_PYTHON_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python${TERMUX_PYTHON_VERSION/./}-crossenv-prefix-$TERMUX_ARCH
 TUR_AUTO_AUDIT_WHEEL=true
+TUR_AUDIT_WHEEL_NO_LIBS=true
 
 source $TERMUX_SCRIPTDIR/common-files/tur_build_wheel.sh
 
@@ -26,8 +27,13 @@ termux_step_pre_configure() {
 
 	export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
 	export GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
-	export GRPC_PYTHON_BUILD_SYSTEM_CARES=1
-	export GRPC_PYTHON_BUILD_SYSTEM_RE2=1
-	export GRPC_PYTHON_BUILD_SYSTEM_ABSL=1
 	export GRPC_PYTHON_BUILD_WITH_CYTHON=1
+
+	mkdir -p $TERMUX_PKG_TMPDIR/_fake_bin
+	sed -e "s|@TERMUX_PREFIX@|${TERMUX_PREFIX}|g" \
+		-e "s|@COMPILER@|$(command -v ${CC})|g" \
+		"$TERMUX_PKG_BUILDER_DIR"/wrapper.py.in \
+		> $TERMUX_PKG_TMPDIR/_fake_bin/"$(basename ${CC})"
+	chmod +x $TERMUX_PKG_TMPDIR/_fake_bin/"$(basename ${CC})"
+	export PATH="$TERMUX_PKG_TMPDIR/_fake_bin:$PATH"
 }

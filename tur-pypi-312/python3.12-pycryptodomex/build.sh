8a0f8940733da864127520260a739ab22c2c39fb
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-21 00:00:53 +0800

    new package: python3.12-pycryptodomex

---
+++ tur-pypi-312/python3.12-pycryptodomex/build.sh
@@ -0,0 +1,43 @@
+TERMUX_PKG_HOMEPAGE=https://www.pycryptodome.org/
+TERMUX_PKG_DESCRIPTION="A self-contained Python package of low-level cryptographic primitives"
+TERMUX_PKG_LICENSE="BSD 2-Clause, Public Domain"
+TERMUX_PKG_LICENSE_FILE="LICENSE.rst"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="3.21.0"
+TERMUX_PKG_SRCURL="https://github.com/Legrandin/pycryptodome/archive/refs/tags/v${TERMUX_PKG_VERSION}x.tar.gz"
+TERMUX_PKG_SHA256=7762d1b658b47e989f21ed844a8bf9a527b130fecec26f0d5076656dc38c0558
+TERMUX_PKG_DEPENDS="python"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
+TERMUX_PKG_BUILD_IN_SRC=true
+TERMUX_PKG_AUTO_UPDATE=true
+
+TERMUX_PYTHON_VERSION=3.12
+TERMUX_PYTHON_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python${TERMUX_PYTHON_VERSION/./}-crossenv-prefix-$TERMUX_ARCH
+TUR_AUTO_AUDIT_WHEEL=true
+TUR_AUDIT_WHEEL_NO_LIBS=true
+
+source $TERMUX_SCRIPTDIR/common-files/tur_build_wheel.sh
+
+termux_step_post_get_source() {
+	export PATH="$TERMUX_PREFIX/opt/python$TERMUX_PYTHON_VERSION/cross/bin:$PATH"
+}
+
+termux_step_pre_configure() {
+	LDFLAGS+=" -Wl,--no-as-needed -lpython${TERMUX_PYTHON_VERSION}"
+}
+
+termux_step_make() {
+	:
+}
+
+termux_step_make_install() {
+	pip install . --prefix=$TERMUX_PREFIX -vv --no-build-isolation --no-deps
+
+	python setup.py bdist_wheel
+}
+
+termux_step_post_make_install() {
+	local _pyver="${TERMUX_PYTHON_VERSION/./}"
+	mv ./dist/pycryptodomex-$TERMUX_PKG_VERSION-cp36-abi3-linux_$TERMUX_ARCH.whl \
+		./dist/pycryptodomex-$TERMUX_PKG_VERSION-cp$_pyver-cp$_pyver-linux_$TERMUX_ARCH.whl
+}

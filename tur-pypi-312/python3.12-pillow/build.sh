e784b53f03c816a9b9410a056289301b27954892
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2024-10-21 01:26:21 +0800

    new package: python3.12-pillow

---
+++ tur-pypi-312/python3.12-pillow/build.sh
@@ -0,0 +1,27 @@
+TERMUX_PKG_HOMEPAGE=https://python-pillow.org/
+TERMUX_PKG_DESCRIPTION="Python Imaging Library"
+TERMUX_PKG_LICENSE="custom"
+TERMUX_PKG_LICENSE_FILE="LICENSE"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="10.4.0"
+TERMUX_PKG_SRCURL=https://github.com/python-pillow/Pillow/archive/refs/tags/${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=e70284e8605a5b7ccb37e5bfd4634598ca2c43c7f2c353572351ccf72c031004
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="freetype, libimagequant, libjpeg-turbo, libraqm, libtiff, libwebp, libxcb, littlecms, openjpeg, python, zlib"
+TERMUX_PKG_PYTHON_COMMON_DEPS="wheel"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PYTHON_VERSION=3.12
+TERMUX_PYTHON_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python${TERMUX_PYTHON_VERSION/./}-crossenv-prefix-$TERMUX_ARCH
+TUR_AUTO_AUDIT_WHEEL=true
+# XXX: Licenses are included in the LICENSE file of Pillow.
+TUR_PACKAGE_WHEEL_LICENSE=false
+TUR_LIB_LICENSE_JSON="force-skipped"
+
+source $TERMUX_SCRIPTDIR/common-files/tur_build_wheel.sh
+
+termux_step_post_make_install() {
+	if [ ! -e "$PYTHON_SITE_PKG/pillow-$TERMUX_PKG_VERSION.dist-info" ]; then
+		termux_error_exit "Package ${TERMUX_PKG_NAME} doesn't build properly."
+	fi
+}

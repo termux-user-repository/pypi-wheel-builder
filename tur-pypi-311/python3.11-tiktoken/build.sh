f3afcd5b9a3d2b1fc793ebd451b24c1d6cc077df
Chongyun Lee <licy183@termux.dev>
2025-03-22 04:01:26 +0800

    python3.11-tiktoken: bump to 0.9.0

--- tur-pypi-311/python3.11-tiktoken/build.sh
+++ tur-pypi-311/python3.11-tiktoken/build.sh
@@ -2,11 +2,11 @@ TERMUX_PKG_HOMEPAGE=https://github.com/openai/tiktoken
 TERMUX_PKG_DESCRIPTION="A fast BPE tokeniser for use with OpenAI's models"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION="0.8.0"
+TERMUX_PKG_VERSION="0.9.0"
 TERMUX_PKG_SRCURL=https://github.com/openai/tiktoken/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
-TERMUX_PKG_SHA256=6ac7a9cfe9f9c4d111ea633b7ae2214ba504a6a4e994b26dff28b3bdee919293
+TERMUX_PKG_SHA256=3ab7d5464350c979a46a036247bfc329ffc783a98f0da6c1fbd3c7a494ef19a6
 TERMUX_PKG_AUTO_UPDATE=true
-TERMUX_PKG_DEPENDS="libc++, python3.10"
+TERMUX_PKG_DEPENDS="libc++, python3.11"
 TERMUX_PKG_PYTHON_COMMON_DEPS="wheel, setuptools-rust"
 TERMUX_PKG_PYTHON_TARGET_DEPS="'regex>=2022.1.18', 'requests>=2.26.0'"
 TERMUX_PKG_BUILD_IN_SRC=true

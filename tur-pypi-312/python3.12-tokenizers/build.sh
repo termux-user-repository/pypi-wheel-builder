8219ac812db0b250d7c233de7d1bf5239c1361c9
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2025-03-21 16:06:05 +0800

    python-tokenizers: fix build

--- tur-pypi-312/python3.12-tokenizers/build.sh
+++ tur-pypi-312/python3.12-tokenizers/build.sh
@@ -51,10 +51,10 @@ termux_step_make_install() {
 	export PYTHONPATH=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
 
 	build-python -m maturin build \
- 				--target $CARGO_BUILD_TARGET \
- 				--release --skip-auditwheel \
- 				--interpreter python${TERMUX_PYTHON_VERSION} \
- 				-Z build-std
+				--target $CARGO_BUILD_TARGET \
+				--release --skip-auditwheel \
+				--interpreter python${TERMUX_PYTHON_VERSION} \
+				-Z build-std
 
 	local _pyver="${TERMUX_PYTHON_VERSION/./}"
 	# Fix wheel name, although it it built with tag `cp39-abi3`, but it is linked against `python3.x.so`

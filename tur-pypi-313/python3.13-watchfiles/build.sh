90bfadc3941cd640003d85c5c306ef065d104879
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2026-04-18 22:37:45 +0800

    new package: python3.13-watchfiles (#272)

---
+++ tur-pypi-313/python3.13-watchfiles/build.sh
@@ -0,0 +1,63 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/samuelcolvin/watchfiles
+TERMUX_PKG_DESCRIPTION="Simple, modern and fast file watching and code reload for Python, written in Rust"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@termux-user-repository"
+TERMUX_PKG_VERSION="1.1.1"
+TERMUX_PKG_SRCURL=https://github.com/samuelcolvin/watchfiles/archive/refs/tags/v$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=48fdf058a68dd763ddb246319f3fd16d6f2a0c713fb89b7c9aa2aca1e39d1dc9
+TERMUX_PKG_AUTO_UPDATE=true
+TERMUX_PKG_DEPENDS="libc++, python, python-pip"
+TERMUX_PKG_PYTHON_COMMON_BUILD_DEPS="wheel"
+TERMUX_PKG_PYTHON_CROSS_BUILD_DEPS="'maturin<1.13'"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+TERMUX_PYTHON_VERSION=3.13
+TERMUX_PYTHON_HOME=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}
+TERMUX_PYTHON_CROSSENV_PREFIX=$TERMUX_PKG_BUILDDIR/python${TERMUX_PYTHON_VERSION/./}-crossenv-prefix-$TERMUX_ARCH
+TERMUX_PYTHON_CROSSENV_BUILDHOME=$TERMUX_PYTHON_CROSSENV_PREFIX/build/lib/python${TERMUX_PYTHON_VERSION}
+TUR_AUTO_AUDIT_WHEEL=true
+TUR_AUDIT_WHEEL_NO_LIBS=true
+TUR_AUTO_BUILD_WHEEL=false
+TUR_WHEEL_DIR="target/wheels"
+
+source $TERMUX_SCRIPTDIR/common-files/tur_build_wheel.sh
+
+termux_step_make() {
+	termux_setup_rust
+
+	export CARGO_BUILD_TARGET=${CARGO_TARGET_NAME}
+	export PYO3_CROSS_PYTHON_VERSION=$TERMUX_PYTHON_VERSION
+	export PYO3_CROSS_LIB_DIR=$TERMUX_PREFIX/lib
+	export PYTHONPATH=$TERMUX_PREFIX/lib/python${TERMUX_PYTHON_VERSION}/site-packages
+	export ANDROID_API_LEVEL="$TERMUX_PKG_API_LEVEL"
+
+	build-python -m maturin build \
+				--target $CARGO_BUILD_TARGET \
+				--release --skip-auditwheel \
+				--interpreter python${TERMUX_PYTHON_VERSION}
+}
+
+termux_step_make_install() {
+	local native_wheel_arch
+	case "$TERMUX_ARCH" in
+		aarch64) native_wheel_arch=arm64_v8a ;;
+		arm)     native_wheel_arch=armeabi_v7a ;;
+		x86_64)  native_wheel_arch=x86_64 ;;
+		i686)    native_wheel_arch=x86 ;;
+		*)
+			echo "ERROR: Unknown architecture: $TERMUX_ARCH"
+			return 1 ;;
+	esac
+	local pack_name="watchfiles"
+	local pyversion="${TERMUX_PYTHON_VERSION/./}"
+	local native_wheel_ext="${TERMUX_PKG_VERSION}-cp${pyversion}-cp${pyversion}-android_${ANDROID_API_LEVEL}_${native_wheel_arch}.whl"
+	local cross_wheel_ext="${TERMUX_PKG_VERSION}-cp${pyversion}-none-any.whl"
+	local release_whl_ext="${TERMUX_PKG_VERSION}-cp${pyversion}-cp${pyversion}-android_${ANDROID_API_LEVEL}_${native_wheel_arch}.whl"
+
+	local _whl_orig="target/wheels/${pack_name}-${native_wheel_ext}"
+	local _whl_dest="target/wheels/${pack_name}-${cross_wheel_ext}"
+	local _whl_release="target/wheels/${pack_name}-${release_whl_ext}"
+	mv "$_whl_orig" "$_whl_dest"
+	pip install --force-reinstall --no-deps --prefix "$TERMUX_PREFIX" "$_whl_dest"
+	mv "$_whl_dest" "$_whl_release"
+}
